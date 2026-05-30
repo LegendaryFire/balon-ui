9a0cb0446bdc9fb4076235243ef5e22a00d3d89f
LegendaryFire <tristan.balon@outlook.com>
2026-05-30 02:23:53 -0700

    Work in progress:

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,15 +3,10 @@ import './Sidebar.css';
 export default class Sidebar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <x-overlay></x-overlay>
-            <div class="sidebar-content" style="height: 100dvh;">
+            <div class="sidebar-content">
                 ${this.innerHTML}
             </div>
+            <x-overlay></x-overlay>
         `
-        const overlayElem = this.querySelector('x-overlay');
-        overlayElem.addEventListener('click', (e) => {
-            e.stopPropagation();
-            this.hidden = true
-        });
     }
 }
