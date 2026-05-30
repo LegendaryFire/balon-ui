9c1d420281337c53d0d13236074afa6424a00ddc
LegendaryFire <tristan.balon@outlook.com>
2026-05-30 03:25:56 -0700

    Add icon library and begin cleanup

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import './Sidebar.css';
+import { createElement, icons } from 'lucide';
 
 export class Sidebar extends HTMLElement {
     connectedCallback() {
@@ -15,5 +16,25 @@ export class Sidebar extends HTMLElement {
     }
 }
 
-export class SidebarHeader extends HTMLElement { }
+export class SidebarHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="sidebar-header-title">${this.innerHTML}</div>
+            <button class="sidebar-header-close" type="button" aria-label="Close"></button>
+        `;
+        const closeElem = this.querySelector('button.sidebar-header-close');
+        closeElem.appendChild(createElement(icons.X));
+        closeElem.addEventListener('click', () => {
+            // Get the sidebar in which the header is located in and set it to hidden to close.
+            const sidebar = this.closest('x-sidebar');
+            if (sidebar) {
+                sidebar.hidden = true;
+            } else {
+                throw new Error('Sidebar was not found in the DOM tree.');
+            }
+        });
+    }
+}
+
 export class SidebarBody extends HTMLElement { }
+export class SidebarFooter extends HTMLElement { }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import Row from './components/Row';
 import Button from './components/Button';
 import { Card, CardHeader, CardBody, CardFooter } from './components/Card';
 import Overlay from './components/Overlay';
-import { Sidebar, SidebarHeader, SidebarBody } from './components/Sidebar';
+import { Sidebar, SidebarHeader, SidebarBody, SidebarFooter } from './components/Sidebar';
 
-class Balon {
+class BalonUi {
     static Content = Content;
     static Row = Row;
     static Button = Button;
@@ -19,18 +19,24 @@ class Balon {
     static Sidebar = Sidebar;
     static SidebarHeader = SidebarHeader;
     static SidebarBody = SidebarBody;
+    static SidebarFooter = SidebarFooter;
+
+    static initializeLibrary() {
+        customElements.define('x-content', BalonUi.Content);
+        customElements.define('x-row', BalonUi.Row);
+        customElements.define('x-button', BalonUi.Button);
+        customElements.define('x-card', BalonUi.Card);
+        customElements.define('x-card-header', BalonUi.CardHeader);
+        customElements.define('x-card-body', BalonUi.CardBody);
+        customElements.define('x-card-footer', BalonUi.CardFooter);
+        customElements.define('x-overlay', BalonUi.Overlay);
+        customElements.define('x-sidebar', BalonUi.Sidebar);
+        customElements.define('x-sidebar-header', BalonUi.SidebarHeader);
+        customElements.define('x-sidebar-body', BalonUi.SidebarBody);
+        customElements.define('x-sidebar-footer', BalonUi.SidebarFooter);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    customElements.define('x-content', Balon.Content);
-    customElements.define('x-row', Balon.Row);
-    customElements.define('x-button', Balon.Button);
-    customElements.define('x-card', Balon.Card);
-    customElements.define('x-card-header', Balon.CardHeader);
-    customElements.define('x-card-body', Balon.CardBody);
-    customElements.define('x-card-footer', Balon.CardFooter);
-    customElements.define('x-overlay', Balon.Overlay);
-    customElements.define('x-sidebar', Balon.Sidebar);
-    customElements.define('x-sidebar-header', Balon.SidebarHeader);
-    customElements.define('x-sidebar-body', Balon.SidebarBody);
+    BalonUi.initializeLibrary();
 });
