6f7fd5943f3a7ec6bb02eb15b907bc82c518dedb
LegendaryFire <tristan.balon@outlook.com>
2026-06-01 21:47:42 -0700

    Cleanup, fine tuning and added components

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import './Navbar.css';
+
+export class Navbar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <nav>
+                ${this.innerHTML}
+            </nav>
+        `;
+    }
+}
+
+export class NavbarLogo extends HTMLElement { }
+
+export class NavbarItem extends HTMLElement {
+    connectedCallback() {
+        const itemVariant = this.getAttribute('variant');
+
+        const itemLink = this.getAttribute('href');
+        this.innerHTML = `<a href="${itemLink}">${this.innerHTML}</a>`;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Button from './components/Button';
 import { Card, CardHeader, CardBody, CardFooter } from './components/Card';
 import Overlay from './components/Overlay';
 import { Sidebar, SidebarHeader, SidebarBody, SidebarFooter } from './components/Sidebar';
+import { Navbar, NavbarLogo, NavbarItem } from './components/Navbar';
 
 export default class BalonUi {
     static Content = Content;
@@ -20,19 +21,25 @@ export default class BalonUi {
     static SidebarHeader = SidebarHeader;
     static SidebarBody = SidebarBody;
     static SidebarFooter = SidebarFooter;
-
-    static initializeLibrary() {
-        customElements.define('x-content', BalonUi.Content);
-        customElements.define('x-row', BalonUi.Row);
-        customElements.define('x-button', BalonUi.Button);
-        customElements.define('x-card', BalonUi.Card);
-        customElements.define('x-card-header', BalonUi.CardHeader);
-        customElements.define('x-card-body', BalonUi.CardBody);
-        customElements.define('x-card-footer', BalonUi.CardFooter);
-        customElements.define('x-overlay', BalonUi.Overlay);
-        customElements.define('x-sidebar', BalonUi.Sidebar);
-        customElements.define('x-sidebar-header', BalonUi.SidebarHeader);
-        customElements.define('x-sidebar-body', BalonUi.SidebarBody);
-        customElements.define('x-sidebar-footer', BalonUi.SidebarFooter);
-    }
+    static Navbar = Navbar;
+    static NavbarLogo = NavbarLogo;
+    static NavbarItem = NavbarItem;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    customElements.define('x-content', BalonUi.Content);
+    customElements.define('x-row', BalonUi.Row);
+    customElements.define('x-button', BalonUi.Button);
+    customElements.define('x-card', BalonUi.Card);
+    customElements.define('x-card-header', BalonUi.CardHeader);
+    customElements.define('x-card-body', BalonUi.CardBody);
+    customElements.define('x-card-footer', BalonUi.CardFooter);
+    customElements.define('x-overlay', BalonUi.Overlay);
+    customElements.define('x-sidebar', BalonUi.Sidebar);
+    customElements.define('x-sidebar-header', BalonUi.SidebarHeader);
+    customElements.define('x-sidebar-body', BalonUi.SidebarBody);
+    customElements.define('x-sidebar-footer', BalonUi.SidebarFooter);
+    customElements.define('x-navbar', BalonUi.Navbar);
+    customElements.define('x-navbar-logo', BalonUi.NavbarLogo);
+    customElements.define('x-navbar-item', BalonUi.NavbarItem);
+});
